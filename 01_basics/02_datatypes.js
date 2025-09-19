ed93bf64ba03e99f7e36be7621e2e2b810bed511
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-09-19 07:54:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("saloni")
+
+let name = "saloni"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigin
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefine => value is not assigned
+//symbol => unique
+
+//object
+console.log(typeof "saloni");
+console.log(typeof null); //object
+console.log(typeof undefine); //undefine
+console.log(typeof 6);
+
+
