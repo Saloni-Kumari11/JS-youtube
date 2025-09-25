24a70d715bd902a85133cf86406df2c5c9d98b78
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-09-25 07:31:17 +0000

    datatypes comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" >1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// the reason is that an equality check == and 
+// comparision > < >= <= work differently.
+//comparisions convert null to a number, treating it as 0
+// That's why null>=0 is true and null>0 is false
+
+
+console.log();
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log();
+// ===
+
+console.log("2"==2);
+console.log("2"===2);
+
+
+
+
