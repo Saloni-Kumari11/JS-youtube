165d5d7641385a3164eebded6dc0eefb7d4b86da
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-09-25 06:06:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+//let score = 33    
+//let score = "33"     // "33" => 33
+//let score = "33abc"  // "33abc" => NaN (not a number)
+//let score = null     // null => 0
+//let score = true     // true => 1 ; false => 0
+//let score = "saloni" //  NaN
+let score = undefined  //Nan
+console.log(typeof score);
+//console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+//let isLoggedIn=1         // true
+//let isLoggedIn=""        // false
+//let isLoggedIn=0         //false
+let isLoggedIn="saloni"  //true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
