93031c9f13ea253fad75d98a719b16a1c4a46cd4
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-09-25 07:15:22 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 //let score = 33    
 //let score = "33"     // "33" => 33
 //let score = "33abc"  // "33abc" => NaN (not a number)
@@ -25,4 +26,45 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+//**************************************Operation ************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-3);
+//  console.log(2*3);
+//  console.log(2**2);
+//  console.log(2/3);
+//  console.log(2%3);
+
+let str1 = "hello "
+let str2 = "saloni"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let a=100
+console.log(a++);
+console.log(++a);
+
+
+
+
+
+
+
+ 
+ 
+ 
+
 
