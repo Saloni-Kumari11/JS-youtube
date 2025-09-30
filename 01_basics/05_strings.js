924a2f3f7364ff97be794d005ce888527b138801
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-09-30 15:01:32 +0000

    Stringsupdated

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,28 +1,54 @@
-const name = "saloni"
-const repoCount = 50
-//console.log(name + repoCount + " Value");
+// const name = "saloni"
+// const repoCount = 50
+// //console.log(name + repoCount + " Value");
 
-//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// //console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('saloni-sk-com')
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(5));
-console.log(gameName.indexOf('a'));
+// const gameName = new String('saloni-sk-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('a'));
 
-const newString = gameName.substring(0,4)
-console.log(newString);
-const anotherString = gameName.slice(-8,4)// -ve values are allowed
-console.log(anotherString);
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+// const anotherString = gameName.slice(-8,4)// -ve values are allowed
+// console.log(anotherString);
 
-const newStringOne = "  saloni  "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "  saloni  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://saloni.com/saloni%20kumari"
-console.log(url.replace('%20','-'));
-console.log(url.includes('sal'));
-console.log(gameName.split('-'));
+// const url = "https://saloni.com/saloni%20kumari"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('sal'));
+// console.log(gameName.split('-'));
 
+const names = new String("SALONI kumAri ")
+console.log(names);
+console.log(names.at(2));
+console.log(names.big());
+console.log(names.anchor("myname"));
+console.log(names.blink(5));
+console.log(names.bold());
+console.log(names.charAt(4));
+console.log(names.charCodeAt(1));
+console.log(names.codePointAt(0));
+console.log(names.concat("hello"));
+console.log(names.endsWith("I"));
+console.log(names.repeat(5));
+console.log(names.slice(2,6));
+console.log(names.search("LO"));
+console.log(names.replace("A","AAA"));
+console.log(names.replaceAll("A","P"));
+console.log(names.toUpperCase());
+console.log(names.toLowerCase());
+console.log(names.toWellFormed());
+console.log(names.sup());
+console.log(names.sub());
+console.log(names.trim());//trim staring and endiing space
+console.log(names.trimEnd());
+console.log(names.trimStart());
+console.log(names.valueOf());
