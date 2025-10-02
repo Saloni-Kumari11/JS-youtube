4c9ebcf616b35068d3d46d2ab74a31742cd75a53
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-10-02 05:21:45 +0000

    nums_maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,28 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100.8)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 23.8966
+//const otherNumber = 123.8966
+//const otherNumber = 1123.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());//international standard
+console.log(hundreds.toLocaleString('en-IN'));//Indian standards
+
+// ++++++++++++++++ Maths +++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.5));
+// console.log(Math);
+// console.log(Math);
+// console.log(Math);
+// console.log(Math);
