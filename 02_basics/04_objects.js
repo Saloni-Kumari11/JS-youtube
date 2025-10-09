29ddb8fa7c1c8bc415aaca66e5f651c954fad977
Saloni Kumari <saloni.kumari.7463sk@gmail.com>
2025-10-09 07:50:32 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,13 @@
+//const tinderUser = new Object() //singleton object
+const tinderUser = {}  //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name ="Sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email:"some@gmail.com"
+    
+}
